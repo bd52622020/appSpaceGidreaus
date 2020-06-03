0297a3ec39d7726e8ada4a68384fb867c4a8400f
giepet <giedrius.petrosiuss@gmail.com>
2020-06-03 09:50:25 +0100

    re assigment

---
+++ week2/assigment_re.py
@@ -0,0 +1,10 @@
+import re
+
+pattern = "We.*Data"
+txt = "We love programming with Big Data"
+
+x = re.search(pattern, txt)
+if x == None:
+    print("Match not found")
+else:
+    print("Match found: ", x.group())
