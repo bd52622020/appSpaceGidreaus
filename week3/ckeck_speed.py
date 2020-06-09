b9aa5a71be2ed4cd659a149ed4e57eda620c01d1
Giedrius <giedrius.petrosiuss@gmail.com>
2020-06-09 10:08:50 +0100

    assigment 7

---
+++ week3/ckeck_speed.py
@@ -0,0 +1,16 @@
+def ckeck_speed(speed):
+     
+    points = int((speed-70) / 5)
+    if speed <= 70:
+        print("Ok")
+    elif points > 12:
+        print("Licence suspended")
+    else:
+        print("Points:", points)
+        
+print("Q1")
+ckeck_speed(68)    
+ckeck_speed(88)
+ckeck_speed(136)
+
+
---
+++ week3/read_file.py
@@ -0,0 +1,17 @@
+import os
+
+def read_file(file_path):
+    try:
+        if os.path.exists(file_path):
+                with open(file_path) as f:
+                        head = [next(f) for x in range(10)]
+                print(head)
+        else:
+                print("File does not exists.")
+    except:
+        print("An exception occured")
+    finally:
+        print("Code finished")
+
+file_path = os.path.join("..","notes.txt")
+read_file(file_path)
