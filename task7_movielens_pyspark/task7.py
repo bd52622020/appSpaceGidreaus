8dbd653aa8f992c18baa4f73f84f44369421cae3
Giedrius <giedrius.petrosiuss@gmail.com>
2020-06-13 11:22:23 +0100

    pyspark results printing adjustments

--- task7_movielens_pyspark/task7.py
+++ task7_movielens_pyspark/task7.py
@@ -8,7 +8,7 @@ import subprocess
 U_ITEM_PATH = "hdfs://sandbox-hdp.hortonworks.com:8020/user/maria_dev/data/u.item2"
 U_DATA_PATH = "hdfs://sandbox-hdp.hortonworks.com:8020/user/maria_dev/data/u.data"
 
-ratings_columns = ["user id", "item id", "rating", "timestamp"]  #original "item id" renamed as "movie id"
+ratings_columns = ["user id", "item id", "rating", "timestamp"]  
 user_columns = ["user id", "age","gender", "occupation", "zip code"]
 movies_columns =  ['movie id', 'movie title', 'release date', 'video release date', 'IMDb URL', 'unknown', 'Action', 'Adventure', 'Animation', "Children's", 'Comedy', 'Crime', 'Documentary', 'Drama', 'Fantasy', 'Film-Noir', 'Horror', 'Musical', 'Mystery', 'Romance', 'Sci-Fi', 'Thriller', 'War', 'Western']
 
@@ -52,13 +52,6 @@ def parseInput(line):
 def parse_input_1(line):
     fields = line.split()
     return (int(fields[1]),  1.0)
-
-def parse_input_2(fields):
-#    fields = line.split("|")
-    if fields[2] == "":
-        return (fields[0],  fields[1], None)
-    else:
-        return (fields[0],  fields[1], datetime.strptime(fields[2], '%d-%b-%Y'))
         
 def parse_input_3(line):
     fields = line.split("|")
@@ -80,7 +73,7 @@ def parse_input_5(line):
 def df_column_sum(df, col):
     return (col, df.select(col).rdd.map(lambda x: (1,int(x[0]))).reduceByKey(lambda x,y: x + y).collect()[0][1])
     
-def get_genres_sums(df, cols, num):
+def get_genres_sums(df, cols):
 
     results = []
     for col in cols:
@@ -99,7 +92,7 @@ if __name__ == "__main__":
     conf = SparkConf().setAppName("WorstMovies")
     sc = SparkContext(conf = conf)
     spark = SparkSession(sc)
-    sc.setLogLevel("WARN")
+    #sc.setLogLevel("WARN")
     
     # Load data
     ratings = sc.textFile("/user/maria_dev/data/u.data")
@@ -114,71 +107,84 @@ if __name__ == "__main__":
     movie_names = load_movie_names_2(U_ITEM_PATH)
     
     
-    ### Q1
+    ### Q1& Q2) Print a list of the 10 movies that received the most number of ratings, sorted by the number of ratings.
     movies_counts = ratings.map(parse_input_1).reduceByKey(lambda x1, x2: x1 + x2).sortBy(lambda x: x[1], ascending=False).take(10)
     results_1 = [ (r[0], r[1], movie_names[r[0]]) for r in movies_counts] # (movie id, movie count, movie title)
      
-    ### Q3
+    ### Q3) Print a list of the number of ratings received by each genre.
     results_3 = get_genres_sums(merged_df, movies_columns[5:])
-    ### Q4
+    
+    ### Q4) Print the oldest movie with a 5 rating.
     movies_ids = load_ratings_5(U_DATA_PATH)
     movies_df2 = movies.map(parse_input_3).toDF(['movie id', 'movie title', 'release date'])
     ind_filter = (movies_df2["movie id"].isin(movies_ids)) & (~movies_df2['release date'].isNull()) 
-    results_4 = = movies_df2.filter(ind_filter).sort(movies_df2["release date"] , ascending=True).take(1)
-    ### Q5
+    results_4 =  movies_df2.filter(ind_filter).sort(movies_df2["release date"] , ascending=True).take(1)
+    
+    ### Q5 Print a list of the genre of the top 10 most rated movies.
     top_rated_movie_ids = [str(r[0]) for r in results_1]
     top_rated_movies_df = movies_df.filter(movies_df["movie id"].isin(top_rated_movie_ids))
     genres_cols = movies_columns[5:]
     top_rated_movies_genres = top_rated_movies_df.select(genres_cols).rdd.map(lambda row: get_movie_genres(row, genres_cols)).collect()
     ids = [id["movie id"] for id in top_rated_movies_df.select(["movie id"]).collect()]
-    results_5 = [(r[2], top_rated_movies_genres[ids.index(str(r[0]))]) for r in results_1]
-    ### Q6
+    results_5 = [(r[2], top_rated_movies_genres[ids.index(str(r[0]))]) for r in results_1] #(movie title, genres)
+    
+    ### Q6 Print the title of the movie that was rated the most by students
     students_id = users.map(parse_input_4).filter(lambda cols: cols[1] == "student").map(lambda col : col[0]).collect()
     students_df = ratings_df.select(["user id", "item id"]).filter(ratings_df["user id"].isin(students_id))
     students_counts = students_df.rdd.map(lambda row: (row[1], 1)).reduceByKey(lambda x1, x2: x1 + x2).sortBy(lambda x: x[1], ascending=False).take(1)[0]
-    ### Q7
-    #rating_5_df = merged_df.select(["item id","rating"]).filter(merged_df["rating"] == 5)
+    
+    ### Q7 Print the list of movies that received the highest number of 5 rating
+    rating_5_df = merged_df.select(["item id","rating"]).filter(merged_df["rating"] == 5)
     results_7 = rating_5_df.rdd.map(lambda row: (row[0], 1)).reduceByKey(lambda x1, x2: x1 + x2).sortBy(lambda x: x[1], ascending=False).take(10)
-    ### Q8
-    results_8 = users_df.select('zip code').groupBy('zip code').count().orderBy('count', ascending=False).show(10)
-    ### Q9
+    
+    ### Q8 Print the list of zip codes corresponding to the highest number of users that rated movies.
+    results_8 = users_df.select('zip code').groupBy('zip code').count().orderBy('count', ascending=False).take(10)
+    
+    ### Q9 Find the most rated movie by users in the age group 20 to 25.
     user_25_30_ids =  [ id["user id"] for id in  users_df.select('user id', 'age').filter(users_df["age"].between(20, 25)).select('user id').collect()]
     movies_id = ratings_df.select(["user id", "item id"]).filter(ratings_df["user id"].isin(user_25_30_ids)).select("item id").groupBy('item id').count().orderBy('count', ascending=False).select("item id").take(1)[0]["item id"]
-    results_9 = movies_df.select('movie id', 'movie title').filter(movies_df["movie id"] == movies_id).select('movie title').collect()
-    ### Q10
+    results_9 = movies_df.select('movie id', 'movie title').filter(movies_df["movie id"] == movies_id).select('movie title').take(10)[0]
+    
+    ### Q10 Print the list of movies that were rate after year 1960.
     ratings_df2 = ratings.map(parse_input_5).toDF(["item id", "date"])
     movies_ids = [ id["item id"] for id in ratings_df2.filter(ratings_df2["date"] > datetime(1960,12,31)).select("item id").collect()]
-    results_10 = movies_df.select(["movie id", "movie title"]).filter(movies_df["movie id"].isin(movies_ids)).collect()
+    results_10 = movies_df.select(["movie id", "movie title"]).filter(movies_df["movie id"].isin(movies_ids)).take(10)
     
+    
+    ### Results print
     print "### Q1, Q2 ###"
     print '==================================================='
     for r in results_1:
         print "{:30} counts : {}".format(r[2], r[1])
-        
-    print "### Q3 ###"
+    print "\n### Q3 ###"
     print '==================================================='
-    print results_3
-    print "### Q4 ###"
+    for r in results_3:
+        print "{:13}: {}".format(r[0], r[1])
+    print "\n### Q4 ###"
     print '==================================================='
     print results_4[0][1]
-    print "### Q5 ###"
+    print "\n### Q5 ###"
     print '==================================================='
     for r in results_5:
         print "{:30} genres : {}".format(r[0], r[1])
-    print "### Q6 ###"
+    print "\n### Q6 ###"
     print '==================================================='
     print movie_names[int(students_counts[0])]
-    print "### Q7 ###"
+    print "\n### Q7 ###"
     print '==================================================='
     print 'First 10 movies with the highest number of 5 rating'
-    print results_7
-    print "### Q8 ###"
+    for r in results_7:
+        print "{:32}: {}".format(movie_names[int(r[0])], r[1])
+    print "\n### Q8 ###"
     print '==================================================='
-    print results_8
-    print "### Q9 ###"
+    for r in results_8:
+        print "zipcode: {:6}  - {}".format(r[0], r[1])
+    print "\n### Q9 ###"
     print '==================================================='
-    print results_9
-    print "### Q10 ###"
+    print results_9["movie title"]
+    print "\n### Q10 ###"
     print '==================================================='
-    print results_10
+    print 'First ten movies which rate after year 1960 (all movies rated after 1960):'
+    for r in results_10:
+        print(r[1])
     
